d7502c4079aa11bafee912b7ea2bf67aa43dc29b
jelena93 <sijan.jelena93@gmail.com>
2018-01-17 00:58:19 +0100

    auth company

---
+++ company-service/src/main/java/company/configuration/CompanyResourceServerConfig.java
@@ -0,0 +1,20 @@
+package company.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@EnableResourceServer
+@EnableWebSecurity
+public class CompanyResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.requestMatchers().antMatchers("/**").and().authorizeRequests().
+                antMatchers("/**").access("hasRole('ROLE_ADMIN')");
+    }
+
+}
