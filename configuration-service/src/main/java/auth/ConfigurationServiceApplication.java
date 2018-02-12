6f15f839a617a88bfbc055966da98f17468dbbf1
jelenas <jelena.sijan@plusplusnt.rs>
2018-02-12 13:18:43 +0100

    configuration-service

---
+++ configuration-service/src/main/java/auth/ConfigurationServiceApplication.java
@@ -0,0 +1,18 @@
+package auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
+public class ConfigurationServiceApplication extends WebMvcConfigurerAdapter {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigurationServiceApplication.class, args);
+    }
+}
